9f1bb8b9d4b35b04ae5bb19af2896e1da0451971
Aurimar <aurimardev@gmail.com>
2022-08-27 02:32:13 +0100

    more changes on Header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,35 +1,16 @@
 import React from "react"
 
-
-            /*
-            For Mobile
-            Drawer/Logo/   /avatarIcon/ShopCardIcon
-                 /     SEARCH BAR    / 
-             */
-            
-
-
-
-                 
-            /*
-            For PC
-            Logo/ SEARCH BAR /avatarIcon/ShopCardIcon
-                / Categorias /
-             */
-            
-
-
 export default function Header() {
     return (
-        <>
-            <div className="bg-pink-300 p-2">
+        <div className="md:container md:mx-auto">
+            <div className="bg-pink-100 p-2 ">
                 <div className="flex flex-row justify-between">
                     <div className="flex flex-row gap-2">
                         <p className="md:invisible">=</p>
                         <p className="text-2xl md:text-4xl">Digital</p>
                     </div>
                      <div className="text-center invisible md:visible md:w-full">
-                    <input placeholder="Search" className="border-2 rounded w-3/4 text-4xl"></input>
+                    <input placeholder="Search" className="border-2 rounded w-3/4 text-4xl"></input> {/*  */}
                 </div>
                     <div className="flex flex-row gap-2 ">
                         <p>Avatar</p>
@@ -37,8 +18,7 @@ export default function Header() {
                     </div>
                 </div>
                 <div className="text-center md:invisible">
-                    <input placeholder="Search" className="border-2 rounded w-1/2  text-2xl "></input>
-                    
+                    <input placeholder="Search" className="border-2 rounded w-2/3  text-2xl "></input> {/*  */}
                 </div>
             </div>
             <div className="flex flex-row justify-center gap-2 invisible md:visible">
@@ -48,6 +28,6 @@ export default function Header() {
                 <p>CategoriaD </p>
             </div>
         
-        </>
+        </div>
     )
 }
