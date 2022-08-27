ac4502c777f66a864c42f87cfd5ebdc140226b9e
Aurimar <aurimardev@gmail.com>
2022-08-27 16:30:49 +0100

    Slider Section Mobile and PC Image Fix ~
    Signed-off-by: Aurimar <aurimardev@gmail.com>

--- components/Slider/RightSideComponent/index.tsx
+++ components/Slider/RightSideComponent/index.tsx
@@ -2,15 +2,24 @@ import Image from "next/image"
 import logonb from '../../../assets/logonb.png'
 
 export default function RightSideComponent() {
+
+  function Imagem() {
+    if (window.innerHeight < 640) {
+      return (
+        <>
+        </>
+      )
+    }
+    else{return<></>}
+  }
+  
     return (
         <>
-        <div className='flex flex-row justify-between '>
-                <div className=' rounded p-2'>
-                  <Image src={logonb} layout={'fixed'} height={100} width={200} alt='TheImage' />
-                </div>
-                <div className="p-2">
-                  <p className="text-sm">Um Longo Texto</p>
-                  <p className="text-sm">Texto</p>
+        <div className='flex flex-row justify-between border-2 m-1 p-1'>
+          <Image src={logonb} layout={'fixed'} height={250} width={500} alt='TheImage' className="rounded p-2" />
+          <div className="p-2 basis-auto">
+                  <p className="text-md">Um Longo Texto</p>
+                  <p className="text-md">Texto</p>
                 </div>
             </div>
         </>
