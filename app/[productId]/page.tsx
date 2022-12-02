777316a8cf7ba27066d16fa26af5803b7a9b7658
Aurimar Jorge de Araujo Antunes Lopes <aurimardev@gmail.com>
2022-12-02 03:47:12 +0000

    some fix + graphql

--- app/[productId]/page.tsx
+++ app/[productId]/page.tsx
@@ -1,12 +1,38 @@
-export default function ProductPage({
-  params,
-}: {
-  params: { productId: string };
-}) {
-  const { productId } = params;
+"use client";
+import { useState } from "react";
+import GodOfWar from "contexts/games/goofwar";
+import SpiderMan from "contexts/games/spiderman";
+import IProduct from "interfaces/IProduct";
+
+interface IProductPage {
+  params?: { productId: string };
+}
+
+export default function ProductPage({ params }: IProductPage) {
+  const productId = params.productId;
+  const Games: IProduct[] = [GodOfWar, SpiderMan];
+  const [Product, setProduct] = useState<IProduct>();
+  console.log(params.productId);
+
   return (
     <div>
-      <p>{productId}</p>
+      {Product && (
+        <>
+          <div key={Product.id}>
+            <div>
+              <p>Image and video caroussel/slider </p>
+              <p>{Product.name}</p>
+              <p>{Product.platform}</p>
+            </div>
+            <div>
+              <p>activation rule</p>
+              <p>Game Info</p>
+              <p>Seller info</p>
+              <p>Price</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
