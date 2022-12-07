846109f1b0962ff09729cf4e84425a3ac596ddf5
Aurimar Jorge de Araujo Antunes Lopes <aurimardev@gmail.com>
2022-12-07 01:18:21 +0000

    husky conf fix

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,4 +3,5 @@ export { TabButton } from './TabButton';
 export { WishlistLoveButton } from './Icons/WishlistLoveButton';
 export { AvatarButton } from './Icons/AvatarButton';
 export { ShopCartButton } from './Icons/ShopCartButton';
+
 export { TextInputDefault } from './TextInput';
